f63a0280ce4444e96960a6ee785922a40dc0e673
Michael La Manna <michael@mlamanna.com>
2025-01-16 12:13:13 -0500

    Update jsonTest.js

--- jsonTest.js
+++ jsonTest.js
@@ -1,32 +1,32 @@
-const QUESTIONS = [
-    {
-      id: 1,
-      export_label: "Sample 1",
-      default_answer: "",
-      text: "Sample Question 1"
-    },
-    {
-      id: 2,
-      export_label: "Sample 2",
-      default_answer: "",
-      text: "Sample Question 2"
-    },
-    {
-      id: 3,
-      export_label: "Sample 3",
-      default_answer: "",
-      text: "Sample Question 3"
-    },
-    {
-      id: 4,
-      export_label: "Sample 4",
-      default_answer: "",
-      text: "Sample Question 4"
-    },
-    {
-      id: 5,
-      export_label: "Sample 5",
-      default_answer: "",
-      text: "Sample Question 5"
-    }
-  ];
+const QUESTIONS = [  
+  {  
+    "id": 1,  
+    "export_label": "Sample 1",  
+    "default_answer": "",  
+    "text": "Sample Question 1"  
+  },  
+  {  
+    "id": 2,  
+    "export_label": "Sample 2",  
+    "default_answer": "",  
+    "text": "Sample Question 2"  
+  },  
+  {  
+    "id": 3,  
+    "export_label": "Sample 3",  
+    "default_answer": "",  
+    "text": "Sample Question 3"  
+  },  
+  {  
+    "id": 4,  
+    "export_label": "Sample 4",  
+    "default_answer": "",  
+    "text": "Sample Question 4"  
+  },  
+  {  
+    "id": 5,  
+    "export_label": "Sample 5",  
+    "default_answer": "",  
+    "text": "Sample Question 5"  
+  }  
+];  
