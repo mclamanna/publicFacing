391e1c892bc6d2c53fb59cefdab9305d52e9ec1e
Michael La Manna <michael@mlamanna.com>
2025-01-16 14:23:41 -0500

    Update jsonTest.js

--- jsonTest.js
+++ jsonTest.js
@@ -1,5 +1,22 @@
-{
-    "id": "question3",
-    "text": "What do you like most about our product?",
-    "type": "text"
-}
+[
+    {
+        "id": "question1",
+        "text": "What do you like most about our product1?",
+        "type": "text"
+    },
+    {
+        "id": "question2",
+        "text": "What do you like most about our product2?",
+        "type": "text"
+    },
+    {
+        "id": "question3",
+        "text": "What do you like most about our product3?",
+        "type": "text"
+    },
+    {
+        "id": "question4",
+        "text": "What do you like most about our product4?",
+        "type": "text"
+    }
+]
